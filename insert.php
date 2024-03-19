8e978565de5253e1c1b2712c82969c7a61191f33
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2024-03-19 08:21:05 +0500

    insert file created
    first query of insert file created and bootstrap link in this file.

---
+++ insert.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Insert Query</title>
+    <!-- bootstrp links -->
+    <link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
+    <script src="./Bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
